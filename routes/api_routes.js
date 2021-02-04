95ce5e2004314be1ca3065d2ec896e036416df6c
Louisa May <mgerwick@gmail.com>
2021-02-04 12:38:55 -0600

    setup post for creating a new workout

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -2,6 +2,16 @@ const router = require('express').Router();
 const db = require('../models');
 const ObjectId = require('mongojs').ObjectId;
 
+router.post('/workouts', (_req, res) => {
+    //create new workout
+    db.Workout.create({}).then((data) => {
+        res.json(data);
+    })
+        .catch((err) => {
+            console.error(err);
+        });
+});
+
 router.get("/workouts", (_req, res) => {
     //get all workouts
     db.Workout.find({}).then((data) => {
