27fc62b3f8f8e98ad12e1a23da991570cf3224a4
Louisa May <mgerwick@gmail.com>
2021-02-04 12:24:10 -0600

    added workout schema to models/workout.js

---
+++ models/workout.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const opts = { toJSON: { virtuals: true } };
+const workoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: Date.now,
+        },
+        excercises: [
+            {
+                type: {
+                    type: String,
+                },
+                weight: {
+                    type: Number,
+                },
+                duration: {
+                    type: Number,
+                },
+                reps: {
+                    type: Number,
+                },
+                sets: {
+                    type: Number,
+                },
+                distance: {
+                    type: Number,
+                },
+            },
+        ],
+    },
+    opts
+);
+
+//totalDuration calculation
+workoutSchema.virtual("totalDuration").get(function () {
+    return this.exercises.reduce(reducer, 0);
+});
+
+const reducer = (accumulator, exercise) => accumulator + excercise.duration;
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
